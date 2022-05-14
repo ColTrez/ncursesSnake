c48a2f4139248369d8978201ce208c2eb1c01ec9
Colin Trezise <ctrezise@asu.edu>
2022-05-14 14:27:10 -0700

    working food implemented

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 #include <ncurses.h>
 #endif
 
+#ifndef __TIME_H__
+#define __TIME_H__
+#include <time.h>
+#endif
+
 #ifndef __STDLIB_H__
 #define __STDLIB_H__
 #include <stdlib.h>
@@ -13,11 +18,9 @@
 #include "snake.h"
 #endif
 
-#define FOOD_INTERVAL 1000
-
 int g_game_speed = 100;
 
-void game_loop(struct Queue* q);
+void game_loop(struct Queue* q, int screenWidth, int screenHeight);
 
 int main(int argc, char* argv[]){
 	
@@ -50,20 +53,24 @@ int main(int argc, char* argv[]){
 
 	refresh();
 
-	game_loop(snake);
+	srand((unsigned int) time(NULL));//seed the random number generator for food placement
+	game_loop(snake, width, height);
 
 	endwin();
 	return 0;
 }
 
-void game_loop(struct Queue* q){
+
+void game_loop(struct Queue* q, int screenWidth, int screenHeight){
 	int ch;
-	int counter = 0;
+	bool foodPlaced = false;
 	
 	//start moving right by default
 	int moveX = 1;
 	int moveY = 0;
 	char headChar = '>';
+	enum direction{UP, DOWN, LEFT, RIGHT};
+	enum direction blocked = LEFT;
 
 	//players can move with arrow keys, wasd, or vi movement controls
 	while((ch = getch()) != KEY_F(1)){
@@ -73,46 +80,82 @@ void game_loop(struct Queue* q){
 			case 'l':
 			case 'd':
 			case KEY_RIGHT:
-				moveX = 1;
-				moveY = 0;
+				if(blocked != RIGHT){
+					moveX = 1;
+					moveY = 0;
+					blocked = LEFT;
+					headChar = '>';
+				}
 				break;
 			case 'h':
 			case 'a':
 			case KEY_LEFT:
-				moveX = -1;
-				moveY = 0;
+				if(blocked != LEFT){
+					moveX = -1;
+					moveY = 0;
+					blocked = RIGHT;
+					headChar = '<';
+				}
 				break;
 			case 'k':
 			case 'w':
 			case KEY_UP:
-				moveX = 0;
-				moveY = -1;
+				if(blocked != UP){
+					moveX = 0;
+					moveY = -1;
+					blocked = DOWN;
+					headChar = '^';
+				}
 				break;
 			case 'j':
 			case 's':
 			case KEY_DOWN:
-				moveX = 0;
-				moveY = 1;
+				if(blocked != DOWN){
+					moveX = 0;
+					moveY = 1;
+					blocked = UP;
+					headChar = 'v';
+				}
 				break;
 		}
 
-		counter++;
-		if (counter == FOOD_INTERVAL){
-			counter = 0;
+		if (!foodPlaced){
 			//todo: make food
+			int foodX = rand() % screenWidth;
+			int foodY = rand() % screenHeight;
+			mvaddch(foodY, foodX, '%');
+			foodPlaced = true;
 		}
 		
 		//move
 		Segment* newSnakeHead = create_segment(q->tail->y + moveY, q->tail->x + moveX);
 		//replace old head char with body char
 		mvaddch(q->tail->y, q->tail->x, ACS_DIAMOND);
-		//replace tail with space
-		Segment* oldSnakeTail = dequeue(q);
-		mvaddch(oldSnakeTail->y, oldSnakeTail->x, ' ');
-		free(oldSnakeTail);
-		//todo: check new head for collision
-		enqueue(q, newSnakeHead);
-		mvaddch(newSnakeHead->y, newSnakeHead->x, headChar);
+		
+		//check new head for collision
+		char atPos = mvinch(newSnakeHead->y, newSnakeHead->x) & A_CHARTEXT;
+		if (atPos == ACS_DIAMOND){
+			//fucking die
+			moveX = 0;
+			moveY = 0;
+			refresh();
+			return;
+		}
+		else if (atPos == '%'){
+			//food
+			enqueue(q, newSnakeHead);
+			mvaddch(newSnakeHead->y, newSnakeHead->x, headChar);
+			foodPlaced = false;
+
+		}
+		else {
+			enqueue(q, newSnakeHead);
+			mvaddch(newSnakeHead->y, newSnakeHead->x, headChar);
+			//replace tail with space
+			Segment* oldSnakeTail = dequeue(q);
+			mvaddch(oldSnakeTail->y, oldSnakeTail->x, ' ');
+			free(oldSnakeTail);
+		}
 
 		refresh();
 	}
